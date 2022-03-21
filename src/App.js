f2e0a390e2a8d64c584e8752f34bb08aead92132
Ajal Jose <ajaljosem@gmail.com>
2022-03-21 15:35:42 +0530

    Rowpost skeleton design completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Banner from "./components/pages/Banner";
 import Header from "./components/pages/Header";
 import './App.css'
+import RowPost from "./components/pages/RowPost";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Banner/>
+      <RowPost/>
     </div>
   );
 }
---
+++ src/components/pages/RowPost.js
@@ -0,0 +1,24 @@
+import React from "react";
+import '../style/RowPost.css'
+function RowPost() {
+  return (
+    <div className="rowPost">
+      <h2 className="title">Originals</h2>
+      <div className="row">
+        <img className="poster" src="https://www.sknmart.com/wp-content/uploads/2020/07/king2.jpg"></img>
+        <img className="poster" src="https://www.sknmart.com/wp-content/uploads/2020/07/king2.jpg"></img>
+        <img className="poster" src="https://www.sknmart.com/wp-content/uploads/2020/07/king2.jpg"></img>
+        <img className="poster" src="https://www.sknmart.com/wp-content/uploads/2020/07/king2.jpg"></img>
+        <img className="poster" src="https://www.sknmart.com/wp-content/uploads/2020/07/king2.jpg"></img>
+        <img className="poster" src="https://www.sknmart.com/wp-content/uploads/2020/07/king2.jpg"></img>
+        <img className="poster" src="https://www.sknmart.com/wp-content/uploads/2020/07/king2.jpg"></img>
+        <img className="poster" src="https://www.sknmart.com/wp-content/uploads/2020/07/king2.jpg"></img>
+        <img className="poster" src="https://www.sknmart.com/wp-content/uploads/2020/07/king2.jpg"></img>
+        <img className="poster" src="https://www.sknmart.com/wp-content/uploads/2020/07/king2.jpg"></img>
+      
+      </div>
+    </div>
+  );
+}
+
+export default RowPost;
