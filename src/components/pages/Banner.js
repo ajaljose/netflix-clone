c01c2dd7447c32dc0b48751bd3c7e4b83939a230
Ajal Jose <ajaljosem@gmail.com>
2022-03-22 22:13:42 +0530

    Banner api integration done: made into dynamic

--- src/components/pages/Banner.js
+++ src/components/pages/Banner.js
@@ -1,21 +1,26 @@
 import "../style/Banner.css";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from '../../axios';
+import {API_KEY,imageURL} from '../../constants/constants';
 function Banner() {
+  const [movie, setMovie] = useState();
+  useEffect(()=>{
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response=>{
+      console.log(response.data.results[0]);
+      setMovie(response.data.results[Math.floor(Math.random() * 10)])
+    }))
+  },[]) 
   return (
-    <div className="Banner">
+    <div style={{backgroundImage:`url(${movie?imageURL+movie.backdrop_path:""})`}} className="Banner">
       
       <div className="content">
-      <h1 className="BannerTitle">Money Heist</h1>
+      <h1 className="BannerTitle">{movie?movie.title:''}</h1>
       <div className="btn_grp">
           <button className="btn">Play</button>
           <button className="btn">MyList</button>
         </div>
         <h1 className="discription">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum
-          blanditiis mollitia, sequi nostrum isteLorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum
-          blanditiis mollitia, sequi nostrum iste
-          
+          {movie?movie.overview:''}
         </h1>
           
       </div>
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY='febb909b6a3392ba631e4085da6bf01e';
+export const imageURL="https://image.tmdb.org/t/p/original";
