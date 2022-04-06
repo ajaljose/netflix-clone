40741176c949885b04e50f131cf7aa7365dae218
Ajal Jose <ajaljosem@gmail.com>
2022-04-06 21:09:37 +0530

    Updates

---
+++ precache-manifest.1b75b4175db3f17dc7f006dda5569771.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ae74528034ddc9d7867",
+    "url": "/netflix-clone/static/css/main.491f26d9.chunk.css"
+  },
+  {
+    "revision": "4ae74528034ddc9d7867",
+    "url": "/netflix-clone/static/js/main.4ae74528.chunk.js"
+  },
+  {
+    "revision": "858ae4873db569d8a42e",
+    "url": "/netflix-clone/static/js/1.858ae487.chunk.js"
+  },
+  {
+    "revision": "0b892cc98d3bb0874451",
+    "url": "/netflix-clone/static/js/runtime~main.0b892cc9.js"
+  },
+  {
+    "revision": "4f6bdf1b4aaa1774e29142cd4cb83ae7",
+    "url": "/netflix-clone/index.html"
+  }
+];
